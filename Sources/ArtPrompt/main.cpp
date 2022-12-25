e5fe19430aeeed9595dee13dee5ac5e822f3305d
gnerkus <ifeanyioraelosi@gmail.com>
2022-12-25 02:54:53 +0100

    display a Twitter emoji

--- Sources/ArtPrompt/main.cpp
+++ Sources/ArtPrompt/main.cpp
@@ -1,26 +1,35 @@
-#include "SFML/include/SFML/Window/Event.hpp"
-#include "SFML/include/SFML/Window/Window.hpp"
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 
 int main()
 {
-    sf::Window window;
+    sf::RenderWindow window;
     window.create(sf::VideoMode(800, 600), "Art Prompt",
                   sf::Style::Titlebar | sf::Style::Close);
 
-    sf::Event newEvent{};
-    bool running = true;
-
-    while (running)
+    while (window.isOpen())
     {
+        sf::Event newEvent;
         while (window.pollEvent(newEvent))
         {
             // Close windows
             if (newEvent.type == sf::Event::Closed)
             {
-                running = false;
+                window.close();
             }
         }
 
+        window.clear(sf::Color::Black);
+
+        sf::Texture texture;
+        if (!texture.loadFromFile(RESOURCES_DIR "/Emoji/2694.png"))
+        {
+        }
+
+        sf::Sprite sprite;
+        sprite.setTexture(texture);
+        window.draw(sprite);
         window.display();
     }
 
