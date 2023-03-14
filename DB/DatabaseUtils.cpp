71cb32818b8c7e3a1453282f9e2d3f6a05668379
gnerkus <ifeanyioraelosi@gmail.com>
2023-03-14 15:07:54 +0100

    remove unused database utils source and header

--- DB/DatabaseUtils.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Ifeanyi Oraelosi on 17.12.21.
-//
-
-#include "DatabaseUtils.h"
-
-DatabaseUtils::DatabaseUtils(std::shared_ptr<DatabaseManager> manager) : QObject() {
-  _manager = manager;
-}
